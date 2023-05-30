7cc9060881d11fe8991849ebfec99ee5c85bfa2a
Mardin <mardin.ezatpana@gmail.com>
2023-05-31 00:07:15 +0330

    add navbar and hero section to project

--- app.vue
+++ app.vue
@@ -1,7 +1,5 @@
 <template>
   <div>
-    <div class="text-sky-500 dark:text-sky-400 text-center">
-        Mardin ep
-      </div>
+    <NuxtPage />
   </div>
 </template>
---
+++ pages/index.vue
@@ -0,0 +1,6 @@
+<template>
+    <div>
+        <Navbar />
+        <CarHero />
+    </div>
+</template>
