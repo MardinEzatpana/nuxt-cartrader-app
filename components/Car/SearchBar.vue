d6af855ab3bde43ce791c45280c19fa520b311ef
Mardin <mardin.ezatpana@gmail.com>
2023-05-31 15:48:02 +0330

    add car and detailCar static views to app

--- components/Car/SearchBar.vue
+++ components/Car/SearchBar.vue
@@ -1,8 +1,19 @@
+<script setup>
+const city = ref("");
+const cityError = ref(false)
+const handleSearch = () => {
+  if (city.value == '') {
+    cityError.value = true;
+  } else {
+    navigateTo(`city/${city.value}/car`)
+  }
+}
+</script>
+
 <template>
-    <div
-    class="
+  <div class="
       font-serif
-      w-[1000px]
+      w-[700px]
       text-2xl
       rounded-full
       bg-white
@@ -11,14 +22,10 @@
       overflow-hidden
       drop-shadow-2xl
       mx-auto
-    "
-  >
-    <input
-      type="text"
-      class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
-      placeholder="Search by city..."
-    />
-    <button class="bg-sky-500 px-10 text-white" >
+    ">
+    <input type="text" class="py-3 px-5 w-full text-2xl rounded-full focus:outline-none"
+      :class="cityError ? 'placeholder-red-500' : ''" placeholder="Search by city..." v-model="city" />
+    <button class="bg-sky-500 px-10 text-white" @click="handleSearch">
       Search
     </button>
   </div>
