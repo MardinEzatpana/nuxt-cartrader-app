e687efce65f5a8e2ed0e134e0ac1201d5e7d3290
Mardin <mardin.ezatpana@gmail.com>
2023-06-01 12:57:07 +0330

    add dynamic to app and handling client and server errors

--- pages/car/[name]-[id].vue
+++ pages/car/[name]-[id].vue
@@ -1,19 +1,30 @@
 <script setup>
 const route = useRoute();
 const { toTitleCase } = useUtilities();
+const {cars} = useCars();
 useHead({
   title: toTitleCase(route.params.name),
 });
 definePageMeta({
   layout: "custom",
 });
+const car = computed(() =>{
+  return cars.find(c =>(
+     c.id === parseInt(route.params.id)
+  ))
+})
 
+if (!car.value) {
+      throw createError({
+        statusCode: 404,
+        message: `Car with ID of ${route.params.id} does not exist`,
+      })};
 </script>
 <template>
-    <div>
-            <CarDetailHero />
-            <CarDetailAttributes />
-            <CarDetailDescription />
+    <div >
+            <CarDetailHero :car="car"/>
+            <CarDetailAttributes :features="car.features"/>
+            <CarDetailDescription :description="car.description"/>
             <CarDetailContact />
     </div>
 </template>
