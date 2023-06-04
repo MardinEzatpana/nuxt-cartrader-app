5d71abfd3876e752769b5ffaea334441e23f0a94
Mardin <mardin.ezatpana@gmail.com>
2023-06-04 12:28:48 +0330

    add authentication and middleware to app

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,3 +1,25 @@
+<script setup>
+const user = useSupabaseUser();
+const supabase = useSupabaseClient();
+
+const logout = async () => {
+
+  const { error } = await supabase.auth.signOut();
+
+  try {
+    await $fetch("/api/_supabase/session", {
+      method: "POST",
+      body: { event: "SIGNED_OUT", session: null },
+    });
+  } catch {
+    return console.log(error);
+  }
+
+  user.value = null;
+  navigateTo("/");
+};
+</script>
+
 <template>
     <header
     class="
@@ -16,5 +38,10 @@
     "
   >
     <NuxtLink class="text-3xl font-mono" to="/">cartrader</NuxtLink>
+    <div v-if="user" class="flex">
+      <NuxtLink to="/profile/listings" class="mr-5">Profile</NuxtLink>
+      <p class="cursor-pointer" @click="logout">Logout</p>
+    </div>
+    <NuxtLink v-else to="/login">Login</NuxtLink>
   </header>
 </template>
